b14d8947504ea795a3893dfb5ba0cc8dff0ecdd8
Pritampk7 <pritamofficial7@gmail.com>
2023-11-13 15:34:25 +0530

    test commit

--- ztest_ztest1/views.py
+++ ztest_ztest1/views.py
@@ -991,6 +991,5 @@ def configureDNA(request):
         
         if serializer.is_valid():
             serializer.save()
-        print("here")
 
         return Response(json.dumps(data, indent=4))
